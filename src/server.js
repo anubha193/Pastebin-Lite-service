c01b347ffb75b5757259e20f4051679c2f802d77
Anubha Rajput <rajputanubha1@gmail.com>
2026-01-29 18:44:54 +0530

    Pastebin Lite – ready for Vercel

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ dotenv.config()
 import app from "./app.js"
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`Server running on ${PORT}`)
+// })
 
 export default app
